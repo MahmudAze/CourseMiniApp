14941c28ed3133baa71c765f017255c3fdb31a0d
MahmudAze <mahmudrahimli2006@gmail.com>
2025-11-11 18:57:58 +0400

    Onion Architecture

---
+++ MiniProject/Domain/Models/Group.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Group : BaseEntity
+    {
+        public string Teacher { get; set; }
+        public string Room { get; set; }
+    }
+}
