ac70be24f8cfa2df4691823b887094fc733c8ed8
MahmudAze <mahmudrahimli2006@gmail.com>
2025-11-11 22:12:27 +0400

    UPDATED

--- MiniProject/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ MiniProject/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae94f67f5a5861b3227ceb821e6e86d38e6c73b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb474b18ce3d6fa8dc3b8ae528dc4eee1f98975")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
